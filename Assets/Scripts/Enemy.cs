e01747db483489bb3725616b93b2c094c9568220
Kamil Bielski <bielskikamil@gmail.com>
2019-12-08 00:25:18 +0100

    palyer attack

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isRunning", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    public void TakeDamage(int damage){
+        
+        health -= damage;
+        Debug.Log("damage TAKEN");
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeBtwAttack <= 0){
+            timeBtwAttack = startTimeBtwAttack;
+
+            if(Input.GetKey(KeyCode.C)){
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+            }
+
+                 
+            }
+        } else {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(attackPos.position, attackRange);
+    }
+}
